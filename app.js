b9bf2eaf9c3182df98cc1366469df9ab24ab3811
Julian So <ch.julian.so@gmail.com>
2021-08-10 22:32:32 -0400

    added db connection file db.js

--- app.js
+++ app.js
@@ -3,3 +3,13 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const passport = require("passport");
 
+// Load config
+// config.env is where all the global variables are stored.
+dotenv.config({ path: "./config/config.env" });
+
+// Initialize app
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on PORT ${PORT}`));
--- config/db.js
+++ config/db.js
@@ -1,2 +1,19 @@
 const moongoose = require("mongoose");
 
+// mongoose deals with promises so async await is necessary.
+const connectDB = async () => {
+  try {
+    const conn = await moongoose.Mongoose.connectDB(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;

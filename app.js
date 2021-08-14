8cc872b068d8a5ab667d71dc90f156b8c641dfd7
Julian So <ch.julian.so@gmail.com>
2021-08-14 11:57:46 -0400

    added DB connection function

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const passport = require("passport");
+const connectDB = require("./config/db");
 
 // Load config
 // config.env is where all the global variables are stored.
 dotenv.config({ path: "./config/config.env" });
+
+// This call connects to mongodb.
+connectDB();
 
 // Initialize app
 const app = express();

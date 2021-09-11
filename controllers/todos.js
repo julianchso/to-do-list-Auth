e2dbc22697727d128488f08448d627b3340e3b6b
Julian So <ch.julian.so@gmail.com>
2021-09-11 19:59:13 -0400

    finished render to do list, now working on delete

--- controllers/todos.js
+++ controllers/todos.js
@@ -2,23 +2,18 @@ const Todo = require("../models/todos");
 
 module.exports = {
   getTodos: async (req, res) => {
-    // console.log(req.user);
-    // console.log(req.user.googleId);
     try {
       const todoItems = await Todo.find({ googleId: req.user.googleId }).lean();
       const itemsLeft = await Todo.countDocuments({
         googleId: req.user.googleId,
         done: false,
       });
-      // console.log(itemsLeft);
-      // console.log(todoItems);
       
       res.render("todos.hbs", {
         todo: todoItems,
         left: itemsLeft,
         name: req.user.firstName
       });
-      console.log(todoItems);
     } catch (err) {
       console.error(err);
     }

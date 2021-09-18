1cd026e5c21da1bcd60b60cff685d64a966fcc8e
Julian So <ch.julian.so@gmail.com>
2021-09-18 19:10:23 -0400

    adding mark incomplete and mark complete

--- controllers/todos.js
+++ controllers/todos.js
@@ -35,12 +35,42 @@ module.exports = {
   },
 
   deleteTodo: async (req, res) => {
-    console.log(req.body);
     try {
       await Todo.findOneAndDelete({ _id: req.body.todoId });
       console.log("Deleted Todo");
       res.json("Deleted Todo");
     } catch (err) {
+      console.log(err);
+    }
+  },
+
+  markComplete: async (req, res) => {
+    console.log("markComplete");
+    try {
+      await Todo.findOneAndUpdate(
+        { _id: req.body.todoId },
+        {
+          done: true,
+        }
+      );
+      console.log("complete");
+      res.json("Marked Complete");
+    } catch {
+      console.log(err);
+    }
+  },
+
+  markIncomplete: async (req, res) => {
+    try {
+      await Todo.findOneAndUpdate(
+        { _id: req.body.todoId },
+        {
+          done: false,
+        }
+      );
+      console.log("Marked Incomplete");
+      res.json("Marked Incomplete");
+    } catch {
       console.log(err);
     }
   },
--- public/js/main.js
+++ public/js/main.js
@@ -6,10 +6,16 @@ Array.from(deleteBtn).forEach((e) => {
   e.addEventListener("click", deleteToDo);
 });
 
+Array.from(todoIncomplete).forEach((e) => {
+  e.addEventListener("click", markComplete);
+});
+
+Array.from(todoComplete).forEach((e) => {
+  e.addEventListener("click", markIncomplete);
+});
+
 async function deleteToDo() {
   const todoId = this.parentNode.dataset.id;
-  // const todoId = this.parentNode.dataset.id;
-  console.log(todoId);
   try {
     const response = await fetch("todos/deleteTodo", {
       method: "delete",
@@ -17,11 +23,33 @@ async function deleteToDo() {
       body: JSON.stringify({
         todoId: todoId,
       }),
-    })
+    });
     const data = await response.json();
     console.log(data);
     location.reload();
   } catch (err) {
     console.log(err);
   }
+}
+
+async function markComplete() {
+  const todoId = this.parentNode.dataset.id;
+  try {
+    const response = await fetch("todos/markComplete", {
+      method: "put",
+      headers: { " Content-type": "application/json" },
+      body: JSON.stringify({
+        todoId: todoId,
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+    location.reload();
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function markIncomplete() {
+  console.log("mark incomplete");
 }
